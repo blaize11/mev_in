86f05a9f8a279d70ee775d0c0d965ea059eadbb4
blaize11 <blairemarchan1@gmail.com>
2025-10-23 00:26:53 +0800

    new added filesz

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HeroSlideController;
-
-Route::post('/hero-slides', [HeroSlideController::class, 'store']);
-Route::get('/hero-slides', [HeroSlideController::class, 'index']);
